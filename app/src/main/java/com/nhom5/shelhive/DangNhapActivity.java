ff0367c1942ed43a63f6ccad5c8d765ab67d3f88
nhxttrxng <nhuttruong11022004@gmail.com>
2025-03-25 13:39:45 +0700

    origin

---
+++ app/src/main/java/com/nhom5/shelhive/DangNhapActivity.java
@@ -0,0 +1,38 @@
+package com.nhom5.shelhive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DangNhapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.dangnhap);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dangnhap), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Xử lý sự kiện nhấn vào TextView "signup"
+        TextView signupTextView = findViewById(R.id.signup);
+        signupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DangNhapActivity.this, DangKyActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/nhom5/shelhive/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nhom5.shelhive;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Mở trực tiếp DangNhapActivity
+        Intent intent = new Intent(MainActivity.this, DangNhapActivity.class);
+        startActivity(intent);
+
+        // Đóng MainActivity nếu không cần quay lại
+        finish();
+    }
+}
